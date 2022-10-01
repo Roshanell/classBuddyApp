f1f95dcdb49904d7e19cab9f80a2b8102f61fb35
Roshanell <96213223+Roshanell@users.noreply.github.com>
2022-10-01 18:59:32 -0500

    update cart total function working

--- public/adjustCart.js
+++ public/adjustCart.js
@@ -1,9 +1,25 @@
-const removeCartItemButton = document.getElementsByClassName('btn btn-error')
-console.log(removeCartItemButton)
-for (let i = 0; i<removeCartItemButton.length; i++){
-  const button = removeCartItemButton[i]
+const removeCartItemButtons = document.getElementsByClassName('btn btn-error')
+for (let i = 0; i<removeCartItemButtons.length; i++){
+  const button = removeCartItemButtons[i]
   button.addEventListener('click', function(event){
     const buttonClicked = event.target
     buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
   })
+}
+
+function updateCartTotal(){
+  const cartItemArea = document.getElementsByClassName('cart-items')[0]
+  const cartRows = cartItemArea.getElementsByClassName('cart-row')
+  const total = 0
+  for (let i = 0; i < cartRows.length; i++){
+    const cartRow = cartRows[i]
+    const priceElement = cartRow.getElementsByClassName('cart-price')[0]
+    const quantityElement =cartRow.getElementsByClassName('cart-quantity-input')[0]
+    const price = priceElement.innerText
+    const quantity = quantityElement.value
+    console.log(quantity * price)
+   
+  }
+
 }
